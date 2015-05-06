47cb4bfcd925ac69b802bd8926d67d2927ffe74a
Paul van der Knaap <p.c.f.vanderknaap@student.tudelft.nl>
2015-05-06 15:11:36 +0200

    Added demo file.

---
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/eis/democode/DemoJSON.java
@@ -0,0 +1,12 @@
+package nl.tudelft.contextproject.eis.democode;
+
+import org.json.JSONObject;
+
+public class DemoJSON {
+
+	private JSONObject obj;
+	
+	public DemoJSON(){
+		obj = new JSONObject();
+	}
+}
