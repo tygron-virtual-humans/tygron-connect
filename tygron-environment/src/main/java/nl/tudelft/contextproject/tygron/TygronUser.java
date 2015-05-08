d76efef93a8c8564808539ae0ac2543a59493a12
nradenovic93 <nradenovic93@gmail.com>
2015-05-08 13:48:10 +0200

    Fixed names and misc.

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronUser.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronUser.java
@@ -7,17 +7,17 @@ public class TygronUser {
 
   private static TygronHttp http;
 
-  private boolean ACCOUNT_ACTIVE = false;
-  private String ACCOUNT_DOMAIN = "";
-  private String ACCOUNT_USERNAME = "";
-  private String ACCOUNT_FIRSTNAME = "";
-  private String ACCOUNT_LASTNAME = "";
-  private String ACCOUNT_NICKNAME = "";
+  private boolean ACTIVE = false;
+  private String DOMAIN = "";
+  private String USERNAME = "";
+  private String FIRSTNAME = "";
+  private String LASTNAME = "";
+  private String NICKNAME = "";
   
   
-  private int ACCOUNT_ID = 0;
-  private long ACCOUNT_LASTLOGIN = 0;
-  private String ACCOUNT_MAXOPTION = "";
+  private int ID = 0;
+  private long LASTLOGIN = 0;
+  private String MAXOPTION = "";
 
   public TygronUser(TygronHttp localhttp) {
     http = localhttp;
@@ -32,54 +32,18 @@ public class TygronUser {
       HttpResponse response = http.requestGet("services/myuser/");
       JSONObject userObj = http.getJsonFromResponse(response);
       
-      ACCOUNT_ACTIVE = (boolean) userObj.get("active");
-      ACCOUNT_DOMAIN = userObj.get("domain").toString();
-      ACCOUNT_USERNAME = userObj.get("userName").toString();
-      ACCOUNT_FIRSTNAME = userObj.get("firstName").toString();
-      ACCOUNT_LASTNAME = userObj.get("lastName").toString();
-      ACCOUNT_NICKNAME = userObj.get("nickName").toString();
+      ACTIVE = Boolean.getBoolean(userObj.getString("active"));
+      DOMAIN = userObj.get("domain").toString();
+      USERNAME = userObj.getString("userName");
+      FIRSTNAME = userObj.getString("firstName");
+      LASTNAME = userObj.getString("lastName");
+      NICKNAME = userObj.getString("nickName");
       
       
-      ACCOUNT_ID = Integer.parseInt(userObj.get("id").toString());
-      ACCOUNT_LASTLOGIN = Long.parseLong(userObj.get("lastLogin").toString());
-      ACCOUNT_MAXOPTION = userObj.get("maxOption").toString();
+      ID = Integer.parseInt(userObj.getString("id"));
+      LASTLOGIN = Long.parseLong(userObj.getString("lastLogin"));
+      MAXOPTION = userObj.getString("maxOption");
     }
   }
-
-public boolean isActive() {
-	return ACCOUNT_ACTIVE;
-}
-
-public String getDomain() {
-	return ACCOUNT_DOMAIN;
-}
-
-public String getUserName() {
-	return ACCOUNT_USERNAME;
-}
-
-public String getFirstName() {
-	return ACCOUNT_FIRSTNAME;
-}
-
-public String getLastName() {
-	return ACCOUNT_LASTNAME;
-}
-
-public String getNickName() {
-	return ACCOUNT_NICKNAME;
-}
-
-public int getID() {
-	return ACCOUNT_ID;
-}
-
-public long getLastLogin() {
-	return ACCOUNT_LASTLOGIN;
-}
-
-public String getMaxOption() {
-	return ACCOUNT_MAXOPTION;
-}
 
 }
