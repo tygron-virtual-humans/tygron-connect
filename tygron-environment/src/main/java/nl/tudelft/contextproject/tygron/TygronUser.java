ab93c9debb4cc3f70e498249fc5c11aabf5e78fc
nradenovic93 <nradenovic93@gmail.com>
2015-05-08 14:21:13 +0200

    Fixed

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronUser.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronUser.java
@@ -4,13 +4,21 @@ import org.json.JSONObject;
 
 public class TygronUser {
 
-  private static TygronHttpConnection http;
+  private static TygronConnection http;
 
-  private boolean ACCOUNT_ACTIVE = false;
-  private String ACCOUNT_DOMAIN = "";
-  private String ACCOUNT_USERNAME = "";
+  private boolean active = false;
+  private String domain = "";
+  private String userName = "";
+  private String firstName = "";
+  private String lastName = "";
+  private String nickName = "";
+  
+  
+  private int id = 0;
+  private long lastLogin = 0;
+  private String maxOption = "";
 
-  public TygronUser(TygronHttpConnection localhttp) {
+  public TygronUser(TygronConnection localhttp) {
     http = localhttp;
   }
 
@@ -19,9 +27,61 @@ public class TygronUser {
    */
   public void loadSettings() {
     if (http != null) {
-      // Todo: insert call to fetch userdata
-      JSONObject userObj = new JSONObject();
+      // Request user info
+      JSONObject userObj = http.callGetEvent("services/myuser/");
+      
+      active = userObj.getBoolean("active");
+      System.out.println(active);
+      domain = userObj.getString("domain");
+      System.out.println(domain);
+      userName = userObj.getString("userName");
+      System.out.println(userName);
+      firstName = userObj.getString("firstName");
+      System.out.println(firstName);
+      lastName = userObj.getString("lastName");
+      nickName = userObj.getString("nickName");
+      
+      
+      id = userObj.getInt("id");
+      lastLogin = userObj.getLong("lastLogin");
+      maxOption = userObj.getString("maxOption");
     }
   }
+
+public boolean isActive() {
+	return active;
+}
+
+public String getDomain() {
+	return domain;
+}
+
+public String getUserName() {
+	return userName;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public String getNickName() {
+	return nickName;
+}
+
+public int getId() {
+	return id;
+}
+
+public long getLastLogin() {
+	return lastLogin;
+}
+
+public String getMaxOption() {
+	return maxOption;
+}
 
 }
