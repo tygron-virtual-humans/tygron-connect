497dd7e60f9f4b9f17dbf584212b63852cf4b3ab
Paul van der Knaap <p.c.f.vanderknaap@student.tudelft.nl>
2015-05-08 12:39:36 +0200

    Updated DemoHttp to use new functionality of TygronHttp

--- tygron-environment/src/main/java/nl/tudelft/contextproject/eis/democode/DemoHttp.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/eis/democode/DemoHttp.java
@@ -5,6 +5,7 @@ import nl.tudelft.contextproject.tygron.TygronSettings;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.impl.client.BasicResponseHandler;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -24,15 +25,9 @@ public class DemoHttp {
 
     // Example GET request
     response = http.requestGet("services/myuser/");
-    try {
-      responseString = new BasicResponseHandler().handleResponse(response);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    System.out.println(responseString);
-    System.out.println(response);
-
+    JSONObject jsResponse = http.getJsonFromResponse(response);
+    System.out.println(jsResponse.get("active"));
+    
     // Example POST request
     response = http.requestPost(
         "services/event/UserServicesEventType/GET_MY_USER/", null);
