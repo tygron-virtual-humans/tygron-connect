9a772e3f178a5d89c9f98d240cf5d1528fa906e3
Harm Griffioen <harmgriffioen@gmail.com>
2015-05-11 10:27:13 +0200

    Deleted instanceof, size already indicates a list

--- tygron-environment/src/test/java/nl/tudelft/contextproject/eis/EntityTest.java
+++ tygron-environment/src/test/java/nl/tudelft/contextproject/eis/EntityTest.java
@@ -2,12 +2,9 @@ package nl.tudelft.contextproject.eis;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.List;
 
 public class EntityTest {
   private static TygronEntity entity;
@@ -21,38 +18,33 @@ public class EntityTest {
   public void test_initIndicator() {
     assertNotNull(entity);
     assertEquals(3,entity.initIndicator().size());
-    assertTrue(entity.initIndicator() instanceof List);
   }
   
   @Test
   public void test_target() {
     assertNotNull(entity);
     assertEquals(2,entity.target().size());
-    assertTrue(entity.target() instanceof List);
   }
   
   @Test
   public void test_otherStakeholders() {
     assertNotNull(entity);
     assertEquals(3,entity.otherStakeholders().size());
-    assertTrue(entity.otherStakeholders() instanceof List);
-    assertTrue(entity.otherStakeholders().get(0) instanceof List);
+    assertEquals(2,entity.otherStakeholders().get(0).size());
   }
   
   @Test
   public void test_progressIndicator() {
     assertNotNull(entity);
     assertEquals(6,entity.progressIndicator().size());
-    assertTrue(entity.progressIndicator() instanceof List);
-    assertTrue(entity.progressIndicator().get(0) instanceof List);
+    assertEquals(2,entity.progressIndicator().get(0).size());
   }
   
   @Test
   public void test_ground() {
     assertNotNull(entity);
     assertEquals(3,entity.ground().size());
-    assertTrue(entity.ground() instanceof List);
-    assertTrue(entity.ground().get(0) instanceof List);
+    assertEquals(2,entity.ground().get(0).size());
   }
   
   @Test
@@ -65,14 +57,12 @@ public class EntityTest {
   public void test_permit() {
     assertNotNull(entity);
     assertEquals(4,entity.permit().size());
-    assertTrue(entity.permit() instanceof List);
   }
   
   @Test
   public void test_stakeholder() {
     assertNotNull(entity);
     assertEquals(2,entity.stakeholder().size());
-    assertTrue(entity.stakeholder() instanceof List);
   }
   
 }
