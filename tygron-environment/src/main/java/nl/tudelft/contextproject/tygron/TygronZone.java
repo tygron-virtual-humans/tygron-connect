98a6a7a460ff761d91a927da3f10d9c96fa49d25
nradenovic93 <nradenovic93@gmail.com>
2015-05-11 10:17:24 +0200

    TygronZone class

---
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronZone.java
@@ -0,0 +1,29 @@
+package nl.tudelft.contextproject.tygron;
+
+import java.util.List;
+import java.util.Map;
+
+public class TygronZone {
+
+	private int allowedFloors;
+	private List<Object> allowedFunctions;
+	private List<Double> categoryFloorM2s;
+	private List<Double> categoryFloorM2sMaquette;
+	private List<Double> categoryM2s;
+	private List<Double> categoryM2sMaquette;
+	private int categoryVersion;
+	private int categoryVersionMaquette;
+	private Map<String, Integer> color;
+	private String description;
+	private List<Integer> detailVersions;
+	private List<List<Double>> details;
+	private int id;
+	private String name;
+	//private ? playable;
+	//private ? polygons;
+	private boolean showlabel;
+	private int sortIndex;
+	private int version;
+	
+	
+}
