966af58b076bbd1f98c22a3c4ca5d81229fb2bb8
KolakCC <KolakCC@gmail.com>
2015-05-11 14:14:05 +0200

    Improved the dummy connection so that it looks for files in /src/*/resources.

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronDummyConnection.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/TygronDummyConnection.java
@@ -2,25 +2,47 @@ package nl.tudelft.contextproject.tygron;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 /**
  * A connection that reads from a file and only returns that response.
- *
+ * 
  */
 public class TygronDummyConnection extends TygronConnection {
   String response;
-  
+
   /**
    * Method that provides a file with a response.
-   * @param inputFile file to be read
+   * 
+   * @param inputFile
+   *          file to be read
    */
   public void setFile(File inputFile) {
     try {
       response = new String(Files.readAllBytes(inputFile.toPath()), StandardCharsets.UTF_8);
     } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Method that provides a file with a response.
+   * 
+   * @param filePath
+   *          file to be read
+   */
+  public void setFile(String filePath) {
+    try {
+      URL resourceUrl = getClass().getResource(filePath);
+      Path resourcePath = Paths.get(resourceUrl.toURI());
+      response = new String(Files.readAllBytes(resourcePath), StandardCharsets.UTF_8);
+    } catch (URISyntaxException | IOException e) {
       throw new RuntimeException(e);
     }
   }
