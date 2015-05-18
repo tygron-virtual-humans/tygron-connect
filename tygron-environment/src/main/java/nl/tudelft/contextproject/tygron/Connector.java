58047c58cfe7f3afe58ce89bfeadb066f9b62745
KolakCC <KolakCC@gmail.com>
2015-05-18 12:40:48 +0200

    Decoupled Connector from Connection

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/Connector.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/Connector.java
@@ -4,39 +4,41 @@ package nl.tudelft.contextproject.tygron;
  * The TygronConnector is the bridge between the Tygron API and JAVA.
  */
 public class Connector {
-
-  private Settings settings;
   private User user;
   private Session session;
   private SessionManager sessionManager;
-  private static HttpConnection connection;
+  private Connection connection;
 
   /**
    * Create a new TygronConnector.
    */
   public Connector() {
-    // Load settings first
-    settings = new Settings();
-
-    // Now load our HTTP Object, it depends on settings
-    connection = new HttpConnection(settings);
-
+    this(new HttpConnection(new Settings()));
+  }
+  
+  /**
+   * Create a new TygronConnector with a given connection.
+   * @param connection the connection to use.
+   */
+  public Connector(Connection connection) {
+    this.connection = connection;
+    
     // Now load user, it depends on http
     user = new User(connection);
 
     // Now load session, it depends on user
     sessionManager = new SessionManager(connection);
-    
+
     session = sessionManager.createOrFindSessionAndJoin("testmap");
   }
-  
+
   /**
    * Return the session manager.
    */
   public SessionManager getSessionManager() {
     return sessionManager;
   }
-  
+
   /**
    * Return the connection manager.
    */
