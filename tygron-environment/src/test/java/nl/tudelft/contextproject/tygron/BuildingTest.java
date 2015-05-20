f04e2057ddb061fba43139b2a3680f8d7d6af50a
Paul van der Knaap <P.C.F.vanderKnaap@student.tudelft.nl>
2015-05-20 09:35:37 +0200

    More fixes

--- tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/BuildingTest.java
+++ tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/BuildingTest.java
@@ -1,15 +1,14 @@
 package nl.tudelft.contextproject.tygron;
 
-import com.esri.core.geometry.Polygon;
-
-import nl.tudelft.contextproject.util.PolygonUtil;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import org.codehaus.jackson.JsonParseException;
+import com.esri.core.geometry.Polygon;
 
+import nl.tudelft.contextproject.util.PolygonUtil;
+
+import org.codehaus.jackson.JsonParseException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
--- tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/IndicatorTest.java
+++ tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/IndicatorTest.java
@@ -2,30 +2,26 @@ package nl.tudelft.contextproject.tygron;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 public class IndicatorTest {
   DummyConnection connection;
-  
+
   @Before
   public void setupConnection() {
     connection = new DummyConnection();
   }
-  
+
   @Test
   public void indicatorTest() {
     connection.setFile("/serverResponses/testmap/lists/indicators.json");
     JSONArray result = connection.callGetEventArray("");
     IndicatorList indicatorList = new IndicatorList(result);
-    
+
     assertEquals("Budget Municipality", indicatorList.get(0).getName());
-    
+
   }
 
 }
