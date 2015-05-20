f0bed8e77e432f1ea96e407e9c352718fe27abcd
JoeHarrison <JoeHarrison1994@gmail.com>
2015-05-20 10:20:46 +0200

    Adding stakeholder tests
    Adding stakeholder tests depending on stakeholders.json

--- tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/StakeholderTest.java
+++ tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/StakeholderTest.java
@@ -10,12 +10,12 @@ import org.junit.Test;
 
 public class StakeholderTest {
   StakeholderList stakeholderList;
-  
+
   @Test
   public void stakeholderTest() {
     assertEquals("Municipality", stakeholderList.get(0).getName());
   }
-  
+
   /**
    * Init test objects.
    */
@@ -24,5 +24,38 @@ public class StakeholderTest {
     String contents = CachedFileReader.getFileContents("/serverResponses/testmap/lists/stakeholders.json");
     JSONArray json = new JSONArray(contents);
     stakeholderList = new StakeholderList(json);
+  }
+
+  @Test
+  public void nameTest() {
+    assertEquals("Municipality", stakeholderList.get(0).getName());
+  }
+
+  @Test
+  public void idTest() {
+    assertEquals(0, stakeholderList.get(0).getId());
+  }
+
+  @Test
+  public void shortNameTest() {
+    assertEquals("Municipality", stakeholderList.get(0).getShortName());
+  }
+
+  @Test
+  public void descriptionTest() {
+    assertEquals("Municipality description", stakeholderList.get(0).getDescription());
+  }
+
+  @Test
+  public void getOwnedLandsTest() {
+    assertEquals("[0, 1, 2]", stakeholderList.get(0).getOwnedLands().toString());
+  }
+
+  @Test
+  public void toStringTest() {
+    String str = "[{\"id\":0,\"name\":\"Municipality\",\"shortName\":\"Municipality\"}"
+        + ", {\"id\":1,\"name\":\"Housing Corporation\",\"shortName\":\"DUWO\"}"
+        + ", {\"id\":2,\"name\":\"Company\",\"shortName\":\"Company\"}]";
+    assertEquals(stakeholderList.toString(), str);
   }
 }
