fbc6ef900014d849a95f5050fc461f3b834647da
KolakCC <KolakCC@gmail.com>
2015-05-20 10:33:55 +0200

    Refactored tests to be more unit-styled.

--- tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/StakeholderTest.java
+++ tygron-environment/src/test/java/nl/tudelft/contextproject/tygron/StakeholderTest.java
@@ -2,24 +2,27 @@ package nl.tudelft.contextproject.tygron;
 
 import static org.junit.Assert.assertEquals;
 
+import nl.tudelft.contextproject.democode.CachedFileReader;
+
 import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 
 public class StakeholderTest {
-  DummyConnection connection;
-  
-  @Before
-  public void setupConnection() {
-    connection = new DummyConnection();
-  }
+  StakeholderList stakeholderList;
   
   @Test
   public void stakeholderTest() {
-    connection.setFile("/serverResponses/testmap/lists/stakeholders.json");
-    JSONArray result = connection.callGetEventArray("");
-    StakeholderList list = new StakeholderList(result);
-    
-    assertEquals("Municipality", list.get(0).getName());
+    assertEquals("Municipality", stakeholderList.get(0).getName());
+  }
+  
+  /**
+   * Init test objects.
+   */
+  @Before
+  public void setupObject() {
+    String contents = CachedFileReader.getFileContents("/serverResponses/testmap/lists/stakeholders.json");
+    JSONArray json = new JSONArray(contents);
+    stakeholderList = new StakeholderList(json);
   }
 }
