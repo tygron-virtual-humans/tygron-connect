60473f200009c9291f8beb6ca213d6f3909a88af
nradenovic93 <nradenovic93@gmail.com>
2015-05-21 21:28:24 +0200

    Get updates

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/api/CallType.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/api/CallType.java
@@ -5,6 +5,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
@@ -16,6 +17,30 @@ public enum CallType {
    * @return a HttpRequestBase that can be executed
    */
   public HttpRequestBase asRequest(JSONArray parameters) {
+    switch (this) {
+      case GET:
+        return new HttpGet();
+      case POST:
+        try {
+          HttpPost ret = new HttpPost();
+          if (parameters != null) {
+            ret.setEntity(new StringEntity(parameters.toString()));
+          }
+          return ret;
+        } catch (UnsupportedEncodingException e) {
+          throw new RuntimeException(e);
+        }
+      default: 
+        throw new RuntimeException("Invalid Type");
+    }    
+  }
+  
+  /**
+   * Creates a HttpRequestBase from this type.
+   * @param parameters parameters to attach to the request, may be null
+   * @return a HttpRequestBase that can be executed
+   */
+  public HttpRequestBase asRequest(JSONObject parameters) {
     switch (this) {
       case GET:
         return new HttpGet();
--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/handlers/JsonObjectResultHandler.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/handlers/JsonObjectResultHandler.java
@@ -5,6 +5,9 @@ import org.json.JSONObject;
 public class JsonObjectResultHandler extends ResultHandler<JSONObject> {
   @Override
   public JSONObject handleResult(String input) {
-    return new JSONObject(input);
+    if(input != null) {
+      return new JSONObject(input);
+    }
+    return null;
   }
 }
