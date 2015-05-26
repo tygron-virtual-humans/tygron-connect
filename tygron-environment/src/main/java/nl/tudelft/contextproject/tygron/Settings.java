5501b3ee169509c2b462744db25e4001377ca8f8
HarmGriffioen <h.j.griffioen@student.tudelft.nl>
2015-05-26 16:29:37 +0200

    Improved the Settings code

--- tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/Settings.java
+++ tygron-environment/src/main/java/nl/tudelft/contextproject/tygron/Settings.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Settings {
-  final private static Logger logger = LoggerFactory.getLogger(Settings.class);
+  private static final Logger logger = LoggerFactory.getLogger(Settings.class);
   private String username;
   private String password;
 
@@ -44,3 +44,4 @@ public class Settings {
     return this.password;
   }
 }
+
