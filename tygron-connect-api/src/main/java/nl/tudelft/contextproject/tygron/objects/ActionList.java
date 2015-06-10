52034b86b60a426fa55ce2798dc0b4da685c0717
nradenovic93 <nradenovic93@gmail.com>
2015-06-10 02:14:47 +0200

    Added action and actionlist classes

---
+++ tygron-connect-api/src/main/java/nl/tudelft/contextproject/tygron/objects/ActionList.java
@@ -0,0 +1,27 @@
+package nl.tudelft.contextproject.tygron.objects;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ActionList extends ArrayList<Action> {
+  
+  /**
+   * Serial ID.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructs a action menu list from a tygron response.
+   * @param input The array to read from.
+   */
+  public ActionList(JSONArray input) {
+    for (int i = 0; i < input.length(); i++) {
+      JSONObject actionWrapper = input.getJSONObject(i);
+      JSONObject actionObj = actionWrapper.getJSONObject("ActionMenu");
+      Action action = new Action(actionObj);
+      this.add(action);
+    }
+  }
+}
