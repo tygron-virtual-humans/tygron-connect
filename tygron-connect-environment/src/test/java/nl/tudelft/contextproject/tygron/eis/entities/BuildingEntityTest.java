59e6a19d7c6a066fd72365a3edf78578766193ba
HarmGriffioen <h.j.griffioen@student.tudelft.nl>
2015-06-17 10:56:39 +0200

    Created building and economy tests

---
+++ tygron-connect-environment/src/test/java/nl/tudelft/contextproject/tygron/eis/entities/BuildingEntityTest.java
@@ -0,0 +1,52 @@
+package nl.tudelft.contextproject.tygron.eis.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import nl.tudelft.contextproject.tygron.eis.TygronPercept;
+import nl.tudelft.contextproject.tygron.objects.Building;
+import nl.tudelft.contextproject.tygron.objects.BuildingList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(value = MockitoJUnitRunner.class)
+public class BuildingEntityTest {
+
+  private BuildingEntity buildingEntity;
+  
+  @Mock
+  private Building building;
+  
+  @Mock
+  private BuildingList buildinglist;
+  
+  /**
+   * Initialize the test, set up mocks.
+   */
+  @Before
+  public void start() {
+    doReturn(1).when(buildinglist).size();
+    doReturn(1).when(building).getId();
+    doReturn("Building1").when(building).getName();
+    doReturn(building).when(buildinglist).get(0);
+    
+    buildingEntity = new BuildingEntity(buildinglist);
+  }
+  
+  @Test
+  public void test() {
+    List<TygronPercept> list = buildingEntity.buildings();
+
+    assertEquals(1,list.size());
+    assertEquals(1,list.get(0).get(0));
+    assertEquals("Building1",list.get(0).get(1));
+  }
+  
+  
+}
