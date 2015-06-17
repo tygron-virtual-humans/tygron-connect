c6e0d90a211eb2d8ad509dc056a9bb2480d7c466
HarmGriffioen <h.j.griffioen@student.tudelft.nl>
2015-06-17 11:10:53 +0200

    Added indicator entity and stakeholder entity tests

---
+++ tygron-connect-environment/src/test/java/nl/tudelft/contextproject/tygron/eis/entities/IndicatorEntityTest.java
@@ -0,0 +1,60 @@
+package nl.tudelft.contextproject.tygron.eis.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import nl.tudelft.contextproject.tygron.eis.TygronPercept;
+import nl.tudelft.contextproject.tygron.objects.indicators.Indicator;
+import nl.tudelft.contextproject.tygron.objects.indicators.IndicatorList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(value = MockitoJUnitRunner.class)
+public class IndicatorEntityTest {
+
+  private IndicatorEntity indicatorEntity;
+  
+  @Mock
+  private Indicator indicator;
+  
+  @Mock
+  private IndicatorList indicatorList;
+  
+  /**
+   * Initialize the test, set up mocks.
+   */
+  @Before
+  public void start() {
+    doReturn(1).when(indicatorList).size();
+    doReturn(1).when(indicator).getId();
+    doReturn("Type").when(indicator).getType();
+    doReturn("Name").when(indicator).getName();
+    doReturn(2.0).when(indicator).getProgress();
+    doReturn(3.0).when(indicator).getCurrent();
+    doReturn(4.0).when(indicator).getTarget();
+    doReturn(indicator).when(indicatorList).get(0);
+    
+    indicatorEntity = new IndicatorEntity(indicatorList);
+  }
+  
+  @Test
+  public void test() {
+    List<TygronPercept> list = indicatorEntity.progressIndicator();
+
+    assertEquals(1,list.size());
+    assertEquals(1,list.get(0).get(0));
+    assertEquals("type",list.get(0).get(1));
+    assertEquals("name",list.get(0).get(2));
+    assertEquals(2.0,list.get(0).get(3));
+    assertEquals(3.0,list.get(0).get(4));
+    assertEquals(4.0,list.get(0).get(5));
+  }
+  
+  
+}
---
+++ tygron-connect-environment/src/test/java/nl/tudelft/contextproject/tygron/eis/entities/StakeholderEntityTest.java
@@ -0,0 +1,77 @@
+package nl.tudelft.contextproject.tygron.eis.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import nl.tudelft.contextproject.tygron.eis.TygronPercept;
+import nl.tudelft.contextproject.tygron.objects.Stakeholder;
+import nl.tudelft.contextproject.tygron.objects.StakeholderList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(value = MockitoJUnitRunner.class)
+public class StakeholderEntityTest {
+
+  private StakeholderEntity stakeholderEntity;
+  
+  @Mock
+  private Stakeholder stakeholder;
+  
+  @Mock
+  private StakeholderList stakeholderList;
+  
+  Map<Integer,Double> weights;
+  
+  /**
+   * Initialize the test, set up mocks.
+   */
+  @Before
+  public void start() {
+    doReturn(1).when(stakeholderList).size();
+    doReturn(1).when(stakeholder).getId();
+    doReturn("shortname").when(stakeholder).getShortName();
+    doReturn("name").when(stakeholder).getName();
+    
+    weights = new HashMap<Integer,Double>();
+    weights.put(1, 1.3);
+    weights.put(2, 0.0);
+    weights.put(3, 4.1);
+    doReturn(weights).when(stakeholder).getIndicatorWeights();
+    
+    doReturn(stakeholder).when(stakeholderList).get(0);
+    
+    stakeholderEntity = new StakeholderEntity(stakeholderList);
+  }
+  
+  @Test
+  public void test_stakeholder() {
+    List<TygronPercept> list = stakeholderEntity.stakeholder();
+
+    assertEquals(1,list.size());
+    assertEquals(1,list.get(0).get(0));
+    assertEquals("name",list.get(0).get(1));
+    assertEquals("shortname",list.get(0).get(2));
+  }
+  
+  @Test
+  public void test_initIndicator() {
+    List<TygronPercept> list = stakeholderEntity.initIndicator();
+
+    assertEquals(2,list.size());
+    assertEquals(1,list.get(0).get(0));
+    assertEquals(1,list.get(1).get(0));
+    assertEquals(1,list.get(0).get(1));
+    assertEquals(3,list.get(1).get(1));
+    assertEquals(1.3,list.get(0).get(2));
+    assertEquals(4.1,list.get(1).get(2));
+  }
+  
+}
